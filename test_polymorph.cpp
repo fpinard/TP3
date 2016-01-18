cee492cf30a227b52a4eb8c8961fee720d0ca324
Ambre Thomas <embrynette@gmail.com>
2016-01-18 17:42:12 +0100

    question 3b

--- test_polymorph.cpp
+++ test_polymorph.cpp
@@ -21,6 +21,7 @@ int main(int argc, char* argv[]) {
   Yoshi       character2 = Yoshi();
   Yoshi*      character4 = new Yoshi();
   Character*  character7 = new Yoshi();
+  Yoshi       character1 = Yoshi(BLUE);
 
   character2.Accelerate();
   character4->Accelerate();
@@ -36,12 +37,16 @@ int main(int argc, char* argv[]) {
   character4->Break();
   character7->Break();
 
+  printf("couleur super belle : %d\n",character1.colour());
+
   printf("Character 2 speed : %f\n", character2.speed());
   printf("Character 4 speed : %f\n", character4->speed());
   printf("Character 7 speed : %f\n", character7->speed());
 
   delete character4;
   delete character7;
+
+  
 
   
 
