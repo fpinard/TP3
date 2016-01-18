79035024047ed333b6f29ff1f0ea080dd9144d59
Ambre Thomas <embrynette@gmail.com>
2016-01-18 17:46:05 +0100

    science bitch

--- Character.cpp
+++ Character.cpp
@@ -19,7 +19,7 @@ int Character::nb_instances = 0;
 //                                Constructors
 // ===========================================================================
 Character::Character() {
-  speed_     = 0.;
+  speed_     = new float(0.);
   max_speed_ = 10.;
   nb_instances ++;
   std::cout<<"nombre instances :"<<nb_instances<<std::endl;
@@ -31,21 +31,22 @@ Character::Character() {
 Character::~Character() {
   nb_instances --;
   std::cout<<"nombre instances :"<<nb_instances<<std::endl;
+  delete speed_;
 }
 
 // ===========================================================================
 //                               Public Methods
 // ===========================================================================
 void Character::Accelerate() {
-  if (speed_ == 0.) speed_ = 0.1;
-  else speed_ *= 1.1;
+  if (*speed_ == 0.) *speed_ = 0.1;
+  else *speed_ *= 1.1;
 
-  if ( speed_ > max_speed_ ) speed_ = max_speed_;
+  if ( *speed_ > max_speed_ ) *speed_ = max_speed_;
 }
 
 void Character::Break() {
-  if (speed_ < 0.1) speed_ = 0.;
-  else speed_ *= 0.9;
+  if (*speed_ < 0.1) *speed_ = 0.;
+  else *speed_ *= 0.9;
 }
 
 // ===========================================================================
