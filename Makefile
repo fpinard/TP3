9e98ed3c6611166a70d59bcde9e73337db5a3db6
Ambre Thomas <embrynette@gmail.com>
2016-01-25 14:28:22 +0100

    Add class Mario

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 all: test_polymorph
 
-test_polymorph: Character.o Yoshi.o test_polymorph.o
-	g++ Character.o Yoshi.o test_polymorph.o -Wall -Wextra -pedantic -o test_polymorph -std=c++11 -g
+test_polymorph: Character.o Yoshi.o Mario.o test_polymorph.o
+	g++ Character.o Yoshi.o Mario.o test_polymorph.o -Wall -Wextra -pedantic -o test_polymorph -std=c++11 -g
 
 Character.o: Character.cpp Character.h
 	g++ -c Character.cpp -Wall -Wextra -pedantic -o Character.o -std=c++11 -g
 
 Yoshi.o: Yoshi.cpp Yoshi.h
 	g++ -c Yoshi.cpp -Wall -Wextra -pedantic -o Yoshi.o -std=c++11 -g
+
+Mario.o: Mario.cpp Mario.h
+	g++ -c Mario.cpp -Wall -Wextra -pedantic -o Mario.o -std=c++11 -g
 
 test_polymorph.o: test_polymorph.cpp Character.h Yoshi.h
 	g++ -c test_polymorph.cpp -Wall -Wextra -pedantic -o test_polymorph.o -std=c++11 -g
--- test_polymorph.cpp
+++ test_polymorph.cpp
@@ -7,6 +7,7 @@
 
 #include "Character.h"
 #include "Yoshi.h"
+#include "Mario.h"
 
 
 // ===========================================================================
@@ -22,33 +23,40 @@ int main(int argc, char* argv[]) {
   Yoshi*      character4 = new Yoshi();
   Character*  character7 = new Yoshi();
   Yoshi       character1 = Yoshi(BLUE);
+  Mario       character0 = Mario();
 
   character2.Accelerate();
   character4->Accelerate();
   character7->Accelerate();
+  character0.Accelerate();
+
+  
 
   printf("Character 2 speed : %f\n", character2.speed());
   printf("Character 4 speed : %f\n", character4->speed());
   printf("Character 7 speed : %f\n", character7->speed());
+  printf("Character 0 speed : %f\n", character0.speed());
+
 
   printf("\n" );
 
   character2.Break();
   character4->Break();
   character7->Break();
+  character0.Break();
+
 
   printf("couleur super belle : %d\n",character1.colour());
 
   printf("Character 2 speed : %f\n", character2.speed());
   printf("Character 4 speed : %f\n", character4->speed());
   printf("Character 7 speed : %f\n", character7->speed());
+  printf("Character 0 speed : %f\n", character0.speed());
+
 
   delete character4;
   delete character7;
 
-  
-
-  
 
   return 0;
 }
